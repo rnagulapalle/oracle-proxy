4b1f457a3691085c2295cf1159f0ace52afeee6e
Nagulapalle, Rajender <rnagulapalle@paypal.com>
2018-07-22 13:09:38 -0700

    rename app

--- src/main/java/com/example/oracleproxy/DemoApplication.java
+++ src/main/java/com/example/oracleproxy/DemoApplication.java
@@ -15,6 +15,7 @@ public class DemoApplication {
       return "Hello World!";
     }
 
+	
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
